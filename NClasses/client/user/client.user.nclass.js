8f607a926022e49b83ce5e3a75734a2f5bf72541
dnblue-code <dntechunity@gmail.com>
2021-05-24 13:02:27 +0700

    update avatar with firebase

--- NClasses/client/user/client.user.nclass.js
+++ NClasses/client/user/client.user.nclass.js
@@ -7,9 +7,7 @@ class{
 
     SetUp(){
         this.CheckIsLoggedIn();
-        if(this.isLoggedIn){
-            this.GetUserPublicData();
-        }
+        
     }
 
     Start(){
@@ -23,32 +21,39 @@ class{
         }
     }
 
-    GetUserPublicData(){
+    async GetUserPublicData(){
         if(this.isLoggedIn){
             var firebase=framework.ImportNClass("client.firebase").firebase;
-            this.userId=localStorage.getItem("user-id");
-            this.userAvatarId=firebase.firestore().collection('users')
+            this.userId=localStorage.getItem("userId");
+            this.userData=(await firebase.firestore().collection('users').doc(this.userId).get()).data();
+            if(this.userId!=null){  
+                this.userAvatarId = this.userData.avatarId;
+            }
+            return this.userAvatarId;
         }
     }
 
-    async GetUserData(){
-        
-    }
+    
 
-    SetAvt(){
+    async SetAvt(){
         this.header=framework.ImportNClass("client.header");
-
         var avt=this.header.avt;
 
-        var avt_index=6;
-
-        var image_url=`/images/avatar/avt-${avt_index}.png`;
-
-        if(!this.isLoggedIn){
-            image_url=`/images/avatar/avt-guess.png`;
+        if(this.isLoggedIn){
+            this.GetUserPublicData()
+            .then(userAvatarId=>{
+                console.log(userAvatarId);
+                var image_url=`/images/avatar/avt-${userAvatarId}.png`;
+                avt.style.backgroundImage=`url(${image_url})`;
+            });            
         }
-
-        avt.style.backgroundImage=`url(${image_url})`;
+        else{
+            
+            image_url=`/images/avatar/avt-guess.png`;
+    
+            avt.style.backgroundImage=`url(${image_url})`;
+        }
+        
 
     }
 
